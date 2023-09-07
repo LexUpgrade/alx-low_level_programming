81f19a809d26733914183fa9ac58b2e9249f04fc
Lex9jar <alexanderedim89@gmail.com>
2023-09-07 14:55:19 +0100

    Prints a string using 'printf'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string using printf
+ * Followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
