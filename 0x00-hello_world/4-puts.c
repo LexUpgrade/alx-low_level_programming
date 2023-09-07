f4c2b35385d5cfa233396c032b6a2c85acf191c5
Lex9jar <alexanderedim89@gmail.com>
2023-09-07 14:43:51 +0100

    Prints a string using 'puts'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints a string
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
