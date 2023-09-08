c787b1e61850a3a720c014eddaf33b58c899d91e
Lex9jar <alexanderedim89@gmail.com>
2023-09-08 17:33:02 +0100

    Prints all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two two-digit numbers
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+		for (j = 48; j <= 56; j++)
+			for (k = 48; k <= 57; k++)
+				for (l = 49; l <= 57; l++)
+				{
+					if (((k + l) > (i + j) && k >= i) || i < k)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+
+						if (i + j + k + l != 227)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+	putchar('\n');
+	return (0);
+}
