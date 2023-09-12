428062a3a9fd597bfcf5494336dbcf944531909c
Lex9jar <alexanderedim89@gmail.com>
2023-09-12 12:29:00 +0100

    Prints the alphabets 10x in lowercase characters

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10x in lowercase
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
