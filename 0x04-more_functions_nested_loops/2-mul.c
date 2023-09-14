35722c7dbcf688b3129dbecaa43bb23790f23b11
Lex9jar <alexanderedim89@gmail.com>
2023-09-14 08:31:48 +0100

    Returns the product of argument a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of a and b
+ * @a: First argument
+ * @b: Second argument
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
