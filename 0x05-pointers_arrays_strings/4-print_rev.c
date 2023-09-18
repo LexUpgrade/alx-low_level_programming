35531ebc39f05a926996b42427a4dc3eaacd8bbd
Lex9jar <alexanderedim89@gmail.com>
2023-09-18 22:51:23 +0100

    Writes the string s and a trailing new line in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,13 @@
  */
 void print_rev(char *s)
 {
-	int len, i, j;
+	int len, i;
 
 	len = 0;
 	while (s[len++] != '0')
 		;
 
-	for (j = len - 1; j >= 0; j--)
-		_putchar(s[j]);
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
