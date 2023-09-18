66b534c0e39612546e4602425a4a619dc93b895e
Lex9jar <alexanderedim89@gmail.com>
2023-09-18 15:58:07 +0100

    Reverses the string 's'

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses the string s
+ * @s: string to reverse
+ *
+ * Return: void.
+ */
+void rev_string(char *s)
+{
+	char tmp;
+	int i, len;
+
+	len = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	
+	for (i = 0; i <= len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1];
+		s[len--] = tmp;
+	}
+}
