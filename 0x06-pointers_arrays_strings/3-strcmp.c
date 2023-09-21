95b15b4957fceda5ffa2512e6c47319a6a300375
Lex9jar <alexanderedim89@gmail.com>
2023-09-21 18:53:34 +0100

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s1: First string
  * @s2: Second string
  *
- * Return: 0 if s1 and s2 are equal, positive number if s1 > s2,
+ * Return: 0 if s1 and s2 are equal or positive number if s1 > s2,
  * or negative number if s1 < s2.
  */
 int _strcmp(char *s1, char *s2)
