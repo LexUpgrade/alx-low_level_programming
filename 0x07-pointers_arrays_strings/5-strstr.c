539047f371890660e1b0896f0013e71529119610
Lex9jar <alexanderedim89@gmail.com>
2023-09-25 18:03:41 +0100

    Locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,16 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, flg;
+	int i, j;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		flg = 0;
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				if (haystack[i + 1] == needle[j + 1])
-					flg++;
+			if (haystack[i + j] != needle[j])
+				break;
 		}
-		if (flg != 0)
+		if (!needle[j])
 			return (&(haystack[i]));
 	}
 	return (NULL);
