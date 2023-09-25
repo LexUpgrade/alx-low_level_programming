98ae2f15eb49792c5af2636d91f31a82b508a057
Lex9jar <alexanderedim89@gmail.com>
2023-09-25 15:09:05 +0100

    Copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,16 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	int len;
 
-	len = 0;
-	while (dest[len] != '\0')
-		len++;
-
-	for (i = 0; src[i] != '\0' && i < n; i++, len++)
-		dest[len] = src[i];
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i] = src[i];
 	for (; i < n; i++)
-		dest[len] = '\0';
+		dest[i] = '\0';
 
 	return (dest);
 }
