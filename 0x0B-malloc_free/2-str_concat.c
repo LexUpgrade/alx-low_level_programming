8693357db6f6ac86359442cce7a481f0a2765c0d
Lex9jar <alexanderedim89@gmail.com>
2023-10-04 14:12:50 +0100

    Returneds a pointer to a newly allocated space in memory which contains the contents of s1, followed by the contents of s2, and null terminated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,22 +2,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - calculates the legnth of a string
- * @s: string to count
- *
- * Return: length of string.
- */
-size_t _strlen(char *s)
-{
-	size_t len;
-
-	len = 0;
-	while (s[len] != '\0')
-		len++;
-	return (len);
-}
-
-/**
  * str_concat - Concatenates two strings
  * @s1: First string
  * @s2: Second string
@@ -31,12 +15,16 @@ char *str_concat(char *s1, char *s2)
 	char *new_str;
 	size_t tot_sz, i, j, len1, len2;
 
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	len1 = 0;
+	while (s1 && s1[len1])
+		len1++;
+	len2 = 0;
+	while (s2 && s2[len2])
+		len2++;
 
 	tot_sz = len1 + len2;
 
-	new_str = malloc((tot_sz + 1) * sizeof(char));
+	new_str = malloc(sizeof(char) * (tot_sz + 1));
 
 	if (new_str == NULL)
 		return (NULL);
