1a4b1e39199bc0a25d4985d2e2fc9b543e6b1183
Lex9jar <alexanderedim89@gmail.com>
2023-10-07 21:36:03 +0100

    Reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,6 +23,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
+	if (!ptr)
+		return (malloc(new_size));
+
 	new_ptr = malloc(new_size);
 	if (new_ptr == NULL)
 		return (NULL);
