2c35d9685aa5797bb7d62a33ebf084bbe6116942
Lex9jar <alexanderedim89@gmail.com>
2023-10-08 20:27:53 +0100

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,9 +10,8 @@
  */
 int is_digit(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
@@ -21,7 +20,6 @@ int is_digit(char *str)
 	}
 	return (1);
 }
-
 /**
  * _strlen - calculates length of a string
  * @str: string to evaluate
@@ -30,15 +28,12 @@ int is_digit(char *str)
  */
 int _strlen(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i] != '\0')
 		i++;
-
 	return (i);
 }
-
 /**
  * _error - handles errors
  */
@@ -47,7 +42,6 @@ void _error(void)
 	printf("Error\n");
 	exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  * @argc: Number of arguments passed
@@ -58,26 +52,19 @@ void _error(void)
 int main(int argc, char **argv)
 {
 	char *str1, *str2;
-	int len, len1, len2, rmd, digit1, digit2, *result, i, flg;
+	int len, len1, len2, rmd, digit1, digit2, *result, i, flg = 0;
 
-	str1 = argv[1];
-	str2 = argv[2];
-
+	str1 = argv[1], str2 = argv[2];
 	if (argc != 3 || !is_digit(str1) || !is_digit(str2))
 		_error();
-
 	len1 = _strlen(str1);
 	len2 = _strlen(str2);
-
 	len = len1 + len2;
-
 	result = malloc(sizeof(int) * (len + 1));
 	if (!result)
 		return (1);
-
 	for (i = 0; i <= len; i++)
 		result[i] = 0;
-
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = str1[len1] - '0';
@@ -92,8 +79,6 @@ int main(int argc, char **argv)
 		if (rmd > 0)
 			result[len1 + len2 + 1] += rmd;
 	}
-	
-	flg = 0;
 	for (i = 0; i < len; i++)
 	{
 		if (result[i])
