873ada5a9df191fd7121f5d43e4ee59a24015bdf
Lex9jar <alexanderedim89@gmail.com>
2023-10-08 20:29:34 +0100

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,7 +4,7 @@
 
 /**
  * is_digit - checks if a string contains a non-digit char
- * @str: string to be evaluated
+ * @str: string to be evaluated.
  *
  * Return: 0 if a non-digit is found, 1 otherwise.
  */
