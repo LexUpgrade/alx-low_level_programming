ab8e23a67190995c0f2404c7a02926fdb8c1d0bc
Lex9jar <alexanderedim89@gmail.com>
2023-10-11 15:58:16 +0100

    Header File

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
 
-void print_name(char *name, void (*f)(char*));
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
 
 #endif /* _FUNCTION_POINTERS_H_ */
