68eb86acf8135147ea9c5427fca3292945ef918b
Lex9jar <alexanderedim89@gmail.com>
2023-10-11 15:50:05 +0100

    Prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to print
+ * @f: a pointer to function
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name)
+		return;
+
+	f(name);
+}
