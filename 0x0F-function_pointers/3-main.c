47fd427c6b0ec61ce223083418d2afd093aa8cc4
Lex9jar <alexanderedim89@gmail.com>
2023-10-11 19:37:36 +0100

    Calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,10 +7,10 @@
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *opr;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -19,22 +19,23 @@ int main(int argc, char **argv)
 	}
 
 	num1 = atoi(argv[1]);
-	opr = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_op_func(opr) == NULL || opr[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*opr == '/' && num2 == 0) ||
-			(*opr == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(opr)(num1, num2));
+
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,11 @@
 #include "3-calc.h"
 
 /**
- * op_add - Returns the sum of two numbers
- * @a: First number
- * @b: Second number
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Sum of a and b.
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
@@ -13,11 +13,11 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - Returns the difference of two numbers
- * @a: First number
- * @b: Second number
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Difference of a and b.
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
@@ -25,11 +25,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - Returns the product of two numbers
- * @a: First number
- * @b: Second Number
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Product of a and b.
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
@@ -37,11 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - Returns the quotient of two numbers
- * @a: First number
- * @b: Second number
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Quotient of a and b.
+ * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
@@ -49,11 +49,11 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - Returns the remainder of the division of two number
- * @a: First number
- * @b: Second number
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: Remainder of the division of a and b.
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
