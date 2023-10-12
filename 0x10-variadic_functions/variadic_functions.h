8ce4bbb3c8ece5342c0053d5ec2335fb6ee6fe0d
Lex9jar <alexanderedim89@gmail.com>
2023-10-12 23:56:56 +0100

    Header File

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,6 @@
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *seperator, const unsigned int n, ...);
-int _putchar(char c);
+void print_strings(const char *seperator, const unsigned int n, ...);
 
 #endif /* _VAR_FUNC_ */
