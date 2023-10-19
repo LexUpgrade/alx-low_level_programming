baf6ace80cdca5ed7c079f6b2a4e3d73a0659ef0
Lex9jar <alexanderedim89@gmail.com>
2023-10-19 23:20:32 +0100

    Prints "Hello, Holberton"

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
-section .data
-	hello db "Hello, Holberton",10,0
+	extern printf
 
-section .text
-	global _start
+	section .data
+msg:		db "Hello, Holberton", 0
+fmt:		db "%s", 10, 0
 
-_start:
-	mov rdi, hello
-	mov rax, 0
-	call printf
+	section .text
 
-	mov rax, 60
-	xor rdi, rdi
-	syscall
-
-section .bss
-	printf resq 1
+	global main
+main:
+	push	rbp
+	mov	rdi,fmt
+	mov	rsi,msg
+	mov	rax,0
+	call	printf
+	pop	rbp
+	mov	rax,0
+	ret
