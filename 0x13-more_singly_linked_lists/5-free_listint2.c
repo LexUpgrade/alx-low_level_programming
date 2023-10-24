975cc403272774dee37a857f9e52a02e90d6ceb9
Lex9jar <alexanderedim89@gmail.com>
2023-10-24 15:02:49 +0100

    Frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return;
 
 	ptr = *head;
 	while (*head)
 	{
 		ptr = *head;
-		(*head) = (*head)->next;
+		*head = (*head)->next;
 		free(ptr);
 	}
 	*head = NULL;
