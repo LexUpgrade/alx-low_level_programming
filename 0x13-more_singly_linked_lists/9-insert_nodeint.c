3325069f7742afd7b477028fa5ca4a3ff63edc1c
Lex9jar <alexanderedim89@gmail.com>
2023-10-25 00:09:44 +0100

    Inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 
 /**
- * node_len - Returns the number of nodes in a single list.
- * @head: A node list
- *
- * Return: Number of nodes in head.
- */
-size_t node_len(listint_t *head)
-{
-	size_t len = 0;
-	listint_t *ptr = head;
-
-	while (ptr)
-	{
-		ptr = ptr->next;
-		len++;
-	}
-	return (len);
-}
-
-/**
  * insert_nodeint_at_index - Inserts a new node at a given position.
  * @head: Pointer to a listint_t list
  * @idx: Position for insertion
@@ -31,23 +12,13 @@ size_t node_len(listint_t *head)
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr, *node;
-	unsigned int count = 1;
+	unsigned int cnt = 0;
 
 	node = malloc(sizeof(listint_t));
-	if (node == NULL)
+	if (node == NULL || *head == NULL)
 		return (NULL);
+
 	node->n = n;
-
-	if (--idx > node_len(*head))
-		return (NULL);
-
-	if (*head == NULL)
-	{
-		node->next = *head;
-		*head = node;
-		return (node);
-	}
-
 	if (idx == 0)
 	{
 		node->next = *head;
@@ -56,13 +27,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	ptr = *head;
-	while (count < idx)
+
+	while (ptr && cnt < idx)
 	{
-		ptr = ptr->next;
-		count++;
+		if (cnt == idx - 1)
+		{
+			node->next = ptr->next;
+			ptr->next = node;
+			return (node);
+		}
+		else
+			ptr = ptr->next;
+		cnt++;
 	}
 
-	node->next = ptr->next;
-	ptr->next = node;
-	return (node);
+	return (NULL);
 }
