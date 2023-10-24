8804b41c449774677a24c17807be3910c8e89112
Lex9jar <alexanderedim89@gmail.com>
2023-10-24 18:09:08 +0100

    Inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -3,44 +3,44 @@
 
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position.
- * @head: A node list.
+ * @head: Pointer to a listint_t list
  * @idx: Position for insertion
- * @n: Data to insert
+ * @n: Data for insertion
  *
- * Return: Address of the new new, or NULL if it fails.
+ * Return: Address of the new node, or NULL if it fails.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr *node;
-	unsigned int i = 1;
-
-	if (*head == NULL)
-		return (NULL);
+	listint_t *ptr, *node;
+	unsigned int count = 1;
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
 
+	if (*head == NULL)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+
 	if (idx == 0)
 	{
 		node->next = *head;
 		*head = node;
-		return (*head);
+		return (node);
 	}
 
 	ptr = *head;
-	while (*head)
+	while (count < idx)
 	{
-		if (i == idx)
-		{
-			node->next = (*head)->next;
-			(*head)->next = node;
-			*head = ptr;
-			return (*head);
-		}
-		i++;
-		*head = (*head)->next;
+		ptr = ptr->next;
+		count++;
 	}
-	return (NULL);
+
+	node->next = ptr->next;
+	ptr->next = node;
+	return (node);
 }
