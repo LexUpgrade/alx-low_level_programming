6401fe3dfb658da0a31173977630bc0fc0b75266
Lex9jar <alexanderedim89@gmail.com>
2023-10-27 21:02:09 +0100

    Sets a bit at given index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,11 +9,14 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
+	unsigned long int i;
 
-	max <<= index;
-	if (max == 0)
+	if (index > 64)
 		return (-1);
-	*n |= max;
+
+	for (i = 1; index > 0; index--, i *= 2)
+		;
+	*n += i;
+
 	return (1);
 }
