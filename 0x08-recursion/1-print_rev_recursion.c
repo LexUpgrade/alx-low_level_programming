969b673e46f118669eb95ecccb7f91ee380616ad
Lex9jar <alexanderedim89@gmail.com>
2023-11-15 14:04:49 +0100

    Prints a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ * @s: String to print
+ *
+ * Return: void.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(++s);
+	_putchar(*s);
+}
