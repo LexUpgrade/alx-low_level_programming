4c4fd43b0ecd2695c0a86c66d7b6c517744d42a6
Lex9jar <alexanderedim89@gmail.com>
2023-11-17 21:36:13 +0100

    Adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_digit - checks if a character is a digit or not
+ * @str: character to check
+ *
+ * Return: 1 if digit, 0 otherwise.
+ */
+int is_digit(char *str)
+{
+	if (*str >= '0' && *str <= '9')
+		return (1);
+	return (0);
+}
+
+/**
+ * main - Prints the addition of all passed positive numbers to the program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0, flg = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		flg = is_digit(argv[i]);
+		if (flg)
+			sum += atoi(argv[i]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
