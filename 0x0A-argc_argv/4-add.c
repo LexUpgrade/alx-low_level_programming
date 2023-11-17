14e339522029808a5146dccdf348d8c71572764a
Lex9jar <alexanderedim89@gmail.com>
2023-11-17 22:55:10 +0100

    Adds all positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,37 +2,15 @@
 #include <stdlib.h>
 
 /**
- * is_digit - checks if a character is a digit or not
- * @str: character to check
- *
- * Return: 1 if digit, 0 otherwise.
- */
-int is_digit(char *str)
-{
-	int flg = 0;
-	while (str)
-	{
-		if (*str >= '0' && *str <= '9')
-			flg = 1;
-		else
-			flg = 0;
-		str++;
-	}
-	if (flg == 0)
-		return (flg);
-	return (flg);
-}
-
-/**
- * main - Prints the addition of all passed positive numbers to the program
+ * main - Adds positive numbers.
  * @argc: Number of arguments passed
- * @argv: Array of arguments passed
+ * @argv: Array of arguments passed.
  *
  * Return: Always 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int i, sum = 0, flg = 0;
+	int i, j, sum = 0;
 
 	if (argc < 2)
 	{
@@ -42,15 +20,20 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		flg = is_digit(argv[i]);
-		if (flg)
-			sum += atoi(argv[i]);
-		else
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				continue;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
+
+	for (i = 1; i < argc; i++)
+		sum += atoi(argv[i]);
 	printf("%d\n", sum);
 	return (0);
 }
