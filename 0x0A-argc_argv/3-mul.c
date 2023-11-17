da987250be0a16ff44f4a0271a1a6c92dae846a4
Lex9jar <alexanderedim89@gmail.com>
2023-11-17 21:32:44 +0100

    Multitplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies arguement values passed to it.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, product = 1;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	product = num1 * num2;
+
+	printf("%d\n", product);
+	return (0);
+}
