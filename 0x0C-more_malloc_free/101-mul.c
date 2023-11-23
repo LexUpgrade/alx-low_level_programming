61d8ae9fe55ce0824cdfb1cbe68024f69a29ca64
Lex9jar <alexanderedim89@gmail.com>
2023-11-23 20:37:03 +0100

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * is_digit - checks if a string contains a non-digit char
+ * is_digit - checks if a string contains a non-digit char.
  * @str: string to be evaluated.
  *
  * Return: 0 if a non-digit is found, 1 otherwise.
