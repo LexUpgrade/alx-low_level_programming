4f8f3bac1a4440b21cf4625ff9501884d17db372
Lex9jar <alexanderedim89@gmail.com>
2023-11-27 18:06:30 +0100

    A dog data struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Dog's data
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
