acd3bed2aabf9aac74fb44a22bdd95bd523946fb
Lex9jar <alexanderedim89@gmail.com>
2023-11-27 18:08:46 +0100

    Header File

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/* STRUCTS */
+
 /**
  * struct dog - Dog's data
  * @name: The name of the dog
@@ -13,5 +15,8 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* FUNCTIONS PROTOTYPES */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* _DOG_H_ */
