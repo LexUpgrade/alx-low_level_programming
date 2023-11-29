0b5020232ab1d75cea1481deb85f349dabd388f9
Lex9jar <alexanderedim89@gmail.com>
2023-11-29 23:11:30 +0100

    Operator Functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The sum of a and b.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference of two integers
+ * @a: First nunber
+ * @b: Seconda number
+ *
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Returns the quotient of two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The quotient of a and b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - The remainder of the division of two integers
+ * @a: First number
+ * @b: Second number
+ *
+ * Return: The remainder of the division of a and b.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
