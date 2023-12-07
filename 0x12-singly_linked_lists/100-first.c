338da05909252c1a671cdd52e2bf6722d853572c
Lex9jar <alexanderedim89@gmail.com>
2023-12-07 20:49:37 +0100

    Prints before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void print_first(void); __attribute__((constructor))
+void print_first(void) __attribute__((constructor));
+
 /**
  * print_first - Executes before the main function
  *
@@ -8,5 +9,6 @@ void print_first(void); __attribute__((constructor))
  */
 void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
