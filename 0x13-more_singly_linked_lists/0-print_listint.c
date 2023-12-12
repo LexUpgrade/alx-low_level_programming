e8baddfc4771589a413b16cbb38c2400789f4853
Lex9jar <alexanderedim89@gmail.com>
2023-12-12 11:14:42 +0100

    Prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	const listint_t *ptr;
-
-	if (h == NULL)
-		return (-1);
-
-	ptr = h;
+	const listint_t *ptr = h;
 
 	while (ptr != NULL)
 	{
