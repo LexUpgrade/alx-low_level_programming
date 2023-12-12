2e5337dd251629864fc8d568f56b483da0d00e8c
Lex9jar <alexanderedim89@gmail.com>
2023-12-12 13:47:39 +0100

    Inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx - 1)
 		{
 			new_node->next = ptr->next;
-			ptr->next = node;
+			ptr->next = new_node;
 			return (new_node);
 		}
 		else
