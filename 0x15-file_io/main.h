761f5c453fcbab378224a704b75ccec6dc281209
Lex9jar <alexanderedim89@gmail.com>
2023-12-18 23:48:57 +0100

    Header File

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* _MAIN_H_ */
