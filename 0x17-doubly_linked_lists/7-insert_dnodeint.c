cfd9d262ad360b9eaeb3c44471dd750a8dcd5f64
Lex9jar <alexanderedim89@gmail.com>
2024-01-25 18:57:25 +0100

    Inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	while (ptr->prev != NULL)
 		ptr = ptr->prev;
 
+	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
@@ -29,7 +30,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			i++;
 		}
 		if (i < idx)
-			return (NULL);
+			return (new);
 		if (ptr->next == NULL)
 			new = add_dnodeint_end(h, n);
 		else
