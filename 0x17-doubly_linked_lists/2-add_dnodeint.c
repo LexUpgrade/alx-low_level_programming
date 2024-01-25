562a801e445edd6d188ce8d5edffc268ef27d939
Lex9jar <alexanderedim89@gmail.com>
2024-01-25 17:07:51 +0100

    Adds a new node at the beginning of a dlistint_t list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,9 +16,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new->prev = NULL;
 	new->n = n;
-	new->next = *head;
 
-	*head = new;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
 
 	return (new);
 }
