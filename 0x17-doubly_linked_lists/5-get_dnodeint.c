a9eecb64eef123f415b410fc1e798476eac70cc4
Lex9jar <alexanderedim89@gmail.com>
2024-01-25 17:43:26 +0100

    Returns the nth node of a dlistint_t linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint - Returns the nth node of a dlistint_t linked list.
+ * @head: A dlistint_t doubly linked list.
+ * @index: Index of the node to return.
+ *
+ * Return: Node derived.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *found, *ptr = head;
+	size_t i = 1;
+
+	if (head == NULL)
+		return (NULL);
+
+	found = malloc(sizeof(dlistint_t));
+	if (found == NULL)
+		return (NULL);
+	found->prev = NULL;
+	found->next = NULL;
+
+	if (index == 0)
+	{
+		found->n = ptr->n;
+	}
+	else
+	{
+		ptr = ptr->next;
+		while (i != index && ptr)
+		{
+			ptr = ptr->next;
+			i++;
+		}
+		found->n = ptr->n;
+	}
+
+	return (found);
+}
