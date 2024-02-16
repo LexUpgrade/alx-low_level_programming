c9a2b2b716be9cd47b1cb952487174efa7bdd26d
Lex9jar <alexanderedim89@gmail.com>
2024-02-16 23:01:50 +0100

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,12 +9,16 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+
 	size_t index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((unsigned const char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
 	for (i = index; ht->array[i]; i++)
 		if (strcmp(ht->array[i]->key, key) == 0)
 			return (ht->array[i]->value);
