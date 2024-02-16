3910a4d708b30070d3ed13dcb6f191c548f66cff
Lex9jar <alexanderedim89@gmail.com>
2024-02-16 22:58:23 +0100

    Retrieves a value associated with a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: A Hash Table
+ * @key: A key for retrieval
+ *
+ * Return: Value associated with 'key', or NULL if not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	size_t index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((unsigned const char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+		if (strcmp(ht->array[i]->key, key) == 0)
+			return (ht->array[i]->value);
+
+	return (NULL);
+}
