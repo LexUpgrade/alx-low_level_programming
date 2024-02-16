07a052b50b274167be7cd80af526551ed533ca32
Lex9jar <alexanderedim89@gmail.com>
2024-02-16 23:12:37 +0100

    Retrieves a value associated with a key

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,8 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 
-	size_t index, i;
+	size_t index;
+	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
@@ -19,9 +20,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 
-	for (i = index; ht->array[i]; i++)
-		if (strcmp(ht->array[i]->key, key) == 0)
-			return (ht->array[i]->value);
+	node = ht->array[index];
+	while (node && (strcmp(node->key, key) != 0))
+		node = node->next;
 
-	return (NULL);
+	return ((node == NULL) ? NULL : node->value);
 }
