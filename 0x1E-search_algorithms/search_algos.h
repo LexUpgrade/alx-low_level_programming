edf8c6ef1d20dac03076f9c2ea8973ace476ef21
LexUpgrade <alexanderedim89@gmail.com>
2024-07-19 14:36:59 +0100

    Header File

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef _SEARCH_ALGOS_
 #define _SEARCH_ALGOS_
 
+#include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
